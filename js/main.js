6a8c1f7cbed1fbe6fcd9c00f588bbac34d96b869
samurai91 <stalker_2012@spaces.ru>
2021-11-22 03:03:04 +0300

    первый слайдер готов

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.features_slider_wrap_block').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true
+    });
+  });
