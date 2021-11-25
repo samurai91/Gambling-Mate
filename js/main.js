ecb8c9495d7a9920b02db9ffaf0f86dd3b2eb6ec
samurai91 <stalker_2012@spaces.ru>
2021-11-25 13:32:27 +0300

    поп-ап доделал

--- js/main.js
+++ js/main.js
@@ -5,14 +5,29 @@ $(document).ready(function(){
         arrows: false,
         dots: true
     });
-  });
-
-  $(document).ready(function(){
     $('.testimonials_slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
         prevArrow: '<div class="testimonials_slider_arrow testimonials_slider_arrow_left">&#8592;</div>',
         nextArrow: '<div class="testimonials_slider_arrow testimonials_slider_arrow_right">&#8594;</div>'
+    });
+    jQuery("a.header_menu_contacts_left_link").click(function () {
+      elementClick = jQuery(this).attr("href")
+      destination = jQuery(elementClick).offset().top - 20;
+      jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+      return false;
+    });
+    $('.popup_btn').on('click', function (events) {
+      event.preventDefault();
+      $('.popup').fadeIn();
+    });
+    $('.popup_close').on('click', function (events) {
+      event.preventDefault();
+      $('.popup').fadeOut();
+    });
+    $('.popup').on('click', function (events) {
+      event.preventDefault();
+      $('.popup').fadeOut();
     });
   });
